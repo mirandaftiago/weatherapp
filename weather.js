e28b7c46cab604fe798b892d7ca106694d89d3dc
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-22 17:04:45 +0000

    removed state variable

--- weather.js
+++ weather.js
@@ -1,21 +1,19 @@
 class Weather {
-  constructor(city, state) {
+  constructor(city) {
     this.apiKey='0561247435c31587d6a0dc66d6764c90';
     this.city = city;
-    this.state = state;
   }
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},&appid=${this.apiKey}`);
     
     const responseData = await response.json();
     return responseData;
   }
 
   // Change weather location
-  changeLocation(city, state) {
+  changeLocation(city) {
     this.city = city;
-    this.state = state;
   }
 }
