082864afea59248d87f55927b0c86cf44d996b6d
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-23 13:30:39 +0000

    change location function

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 // Init weather object
-const weather = new Weather('Porto', 'Portugal');
+const weather = new Weather('Porto');
 // Init ui
 const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+
+  weather.changeLocation('Lisboa');
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
 
 function getWeather(){
   weather.getWeather()
