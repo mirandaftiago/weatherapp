146509f5a501029cea0375572fbcf0082c5b7581
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-23 09:35:49 +0000

    fetch data from API

---
+++ app.js
@@ -0,0 +1,17 @@
+// Init weather object
+const weather = new Weather('Porto', '');
+// Init ui
+const ui = new UI();
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// weather.changeLocation('Miami', 'FL');
+
+function getWeather(){
+  weather.getWeather()
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => console.log(err));
+}
--- weather.js
+++ weather.js
@@ -1,19 +1,22 @@
 class Weather {
-  constructor(city) {
-    this.apiKey='0561247435c31587d6a0dc66d6764c90';
+  constructor(city, state) {
+    this.apiKey = '0561247435c31587d6a0dc66d6764c90';
     this.city = city;
+    this.state = state;
   }
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},&appid=${this.apiKey}`);
-    
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`);
+
     const responseData = await response.json();
+
     return responseData;
   }
 
   // Change weather location
-  changeLocation(city) {
+  changeLocation(city, state) {
     this.city = city;
+    this.state = state;
   }
 }
